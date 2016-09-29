d449371da5c84177aaab2a3388d18d690f3f2260
Pedro Alvarez <pedro.alvarez@codethink.co.uk>
2016-09-29 09:42:14 +0100

    Make haproxy use /data/haproxy.cfg if found

--- docker/haproxy/start.sh
+++ docker/haproxy/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if [ -e /data/haproxy.cfg ]; then
+    cp /data/haproxy.cfg /etc/haproxy/haproxy.cfg
+fi
+
 . /etc/sysconfig/haproxy
 haproxy-systemd-wrapper -f /etc/haproxy/haproxy.cfg -p /run/haproxy.pid $OPTIONS
