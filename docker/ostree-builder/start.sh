9547473e659d4c575c372613678aaff2438f05f0
Pedro Alvarez <pedro.alvarez@codethink.co.uk>
2016-11-07 17:25:38 +0000

    Upgrade to f24

--- docker/ostree-builder/start.sh
+++ docker/ostree-builder/start.sh
@@ -5,13 +5,13 @@ set -x
 
 mkdir -p /home/working
 git clone https://github.com/CentOS/sig-atomic-buildscripts /home/working/sig-atomic-buildscripts
-git clone https://git.fedorahosted.org/git/fedora-atomic.git -b f23 /home/working/fedora-atomic
+git clone https://git.fedorahosted.org/git/fedora-atomic.git -b f24 /home/working/fedora-atomic
 
 ostree --repo=/data/repo init --mode=archive-z2
 
 cd /home/working/fedora-atomic/
-curl -o fedora-23-updates.repo https://git.fedorahosted.org/cgit/fedora-repos.git/plain/fedora-updates.repo?h=f23
-sed -i -e 's/\$releasever/23/g' fedora-23-updates.repo
+curl -o fedora-24-updates.repo https://git.fedorahosted.org/cgit/fedora-repos.git/plain/fedora-updates.repo?h=f24
+sed -i -e 's/\$releasever/24/g' fedora-24-updates.repo
 cp /workdir/*.json /home/working/fedora-atomic/
 cp /workdir/treecompose-var-post.sh /home/working/fedora-atomic/
 rpm-ostree compose tree --repo=/data/repo /home/working/fedora-atomic/fedora-atomic-yarn-runner.json
@@ -21,13 +21,13 @@ rpm-ostree compose tree --repo=/data/repo /home/working/fedora-atomic/fedora-ato
 
 # Fixes needed for gitserver
 cd /workdir
-ostree --repo=/data/repo checkout -U fedora-atomic/f23/x86_64/docker-gitserver gitserver
+ostree --repo=/data/repo checkout -U fedora-atomic/f24/x86_64/docker-gitserver gitserver
 mv gitserver/usr/etc gitserver/etc            # Put /etc in the right place
 rm -r gitserver/usr/var/lib                   # Remove /usr/var/lib to avoid conflicts
 rm -r `find gitserver/usr/var -type d -empty` # Remove empty folders from /usr/var
 mv gitserver/usr/var/* gitserver/var/         # Move /usr/var contents to /var
 rm -r gitserver/usr/var                       # Remove what's left from /usr/var
-ostree --repo=/data/repo commit -b fedora-atomic/f23/x86_64/docker-gitserver --link-checkout-speedup gitserver
+ostree --repo=/data/repo commit -b fedora-atomic/f24/x86_64/docker-gitserver --link-checkout-speedup gitserver
 
 # Fixes needed for yarn runner
 cd /workdir
