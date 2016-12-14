b3e2bbe9401c94e285c2112030ab7cd727f86df6
Zara Zaimeche <zara.zaimeche@codethink.co.uk>
2016-12-14 13:13:38 +0000

    Create sphinx directory layout when start.sh runs
    This adds a line to the start.sh script so that sphinx 'build' and
    'source' directories will be created when the containers first run.
    This currently does not check for existing sphinx directories, as
    sphinx does this for us; we may wish to add our own check to make
    this more robust.
    
    It also copies the sphinx config to an 'immutable' config directory
    so we can keep it separate from the source until build time,
    in order to regenerate docs with each push to master.

--- docker/git-server/start.sh
+++ docker/git-server/start.sh
@@ -44,6 +44,16 @@ else
     fi
 fi
 
+# Create docs directory structure (sphinx requires an author name and
+# version number, so we repeat the project name and use '1' by default for now)
+sphinx-quickstart -q --sep --project=PROJECT_NAME --author=PROJECT_NAME -v=1 /data
+
+# we want to keep our config pristine so we can delete docs, so we copy it
+
+mkdir /data/sphinx-config
+
+cp -r /data/source/* /data/sphinx-config
+
 #fix ownership of git repos
 chown -R apache:apache /data
 
